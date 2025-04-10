75cc4c72f295086c9e963da3c3f07f5b81f588cf
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-10 23:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let sum = document.getElementById('sum');
+let btn = document.getElementById('sum_btn');
+let i1 = document.getElementById('n1');
+let i2 = document.getElementById('n2');
+
+btn.addEventlistener('click',()=>{
+	let val1 = i1.value;
+	let val2 = i2.value;
+	let total = val1+val2;
+	sum.innerText = `${total}`;
+})
